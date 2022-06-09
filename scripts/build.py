6c164a9ede859cd72af3a594ed62a738443a5bf1
Francisco N. C. Sobral <fsobral@uem.br>
2022-06-08 23:18:28 -0300

    Improved script.

--- scripts/build.py
+++ scripts/build.py
@@ -7,7 +7,7 @@ abbr = {"Segunda": "seg",
         "Quinta": "qui",
         "Sexta": "sex"}
 
-schedule = pd.read_csv("organizacao.csv", dtype=str)
+schedule = pd.read_csv("schedule.tsv", dtype=str, delimiter="\t")
 
 rooms_for_a_day = {}
 
@@ -15,7 +15,13 @@ currRoom = None
 
 currDay = None
 
-days = []
+days = [] # list
+
+talks = []
+
+rooms = {} # dict
+
+speakers = {}
 
 for i, row in schedule.iterrows():
     
@@ -23,6 +29,17 @@ for i, row in schedule.iterrows():
 
         currDay  = row["Dia"]
         currRoom = row["Local"].strip()
+
+        # Add the room to the list of rooms. In case the same room
+        # appears twice, keep the last information
+        rooms[currRoom] = {
+            "name": currRoom,
+            "hide": False,
+            "links": [
+                {"name": "Assistir", "absolute_url": row["Link"],
+                 "icon":"play"}
+            ]
+        }
 
         # If is changing the day and is not the first row, then flush
         # all the information about the rooms to the current day
@@ -42,15 +59,51 @@ for i, row in schedule.iterrows():
     if (not pd.isna(row["Local"])) and (row["Local"] != currRoom):
 
         currRoom = row["Local"].strip()
+
+        # Add the room to the list of rooms. In case the same room
+        # appears twice, keep the last information
+        rooms[currRoom] = {
+            "name": currRoom,
+            "hide": False,
+            "links": [
+                {"name": "Assistir", "absolute_url": row["Link"],
+                 "icon":"play"}
+            ]
+        }
         
     if currRoom not in rooms_for_a_day:
 
         rooms_for_a_day[currRoom] = {"name": currRoom,
                                      "talks": []}
 
+    # Talks
+    talk = {
+        "meta" :
+        {
+            "name": row["Titulo"],
+            "categories": list(map(str.strip, row["Tipo"].split(';'))),
+            "speakers": list(map(str.strip, row["Palestrantes"].split(';'))),
+            "links": rooms[currRoom]["links"],
+            "live":  rooms[currRoom]["links"]
+        },
+        "abstract": row["Resumo"]
+    }
+
+    talks.append(talk)
+
+    # Speaker
+
+    for name in talk["meta"]["speakers"]:
+    
+        speakers[name] = {
+            "name": name,
+            "first_name": "",
+            "last_name" : name
+        }
+
     # Create a talk in that room
     rooms_for_a_day[currRoom]["talks"].append(
-        {"name": row["Titulo"].strip(),
+        {"name": talk["meta"]["name"],
          "time_start": row["Inicio"],
          "time_end":   row["Fim"]}
         )
@@ -59,66 +112,42 @@ day["rooms"] = list(rooms_for_a_day.values())
 
 days.append(day)
 
+# Save program
 with open("../_data/program.yml", "w") as fp:
 
     fp.write(yaml.dump({"days": days}, allow_unicode=True))
 
-
 # Generate rooms files
 # ####################
-rooms = pd.read_csv("salas.csv")
 
-for i, row in rooms.iterrows():
-
-    room = {"name": row["Nome"].strip(),
-            "hide": True}
-            #"live": {"absolute_url": row["Link"]}}
+for i, r in enumerate(rooms.values()):
 
     with open("../_rooms/room{0:02d}.md".format(i), "w") as fp:
 
         fp.write("---\n")
-        fp.write(yaml.dump(room, allow_unicode=True))
+        fp.write(yaml.dump(r, allow_unicode=True))
         fp.write("---\n")
 
 
 # Generate talks files
 # ####################
 
-talks = pd.read_csv("palestras.csv")
-
-for i, row in talks.iterrows():
-
-    talk = {"name": row["Titulo"],
-            "categories": list(map(str.strip, row["Tipo"].split(';'))),
-            "speakers": list(map(str.strip, row["Palestrantes"].split(';')))
-            }
+for i, t in enumerate(talks):
 
     with open("../_talks/talk{0:02d}.md".format(i), "w") as fp:
     
         fp.write("---\n")
-        fp.write(yaml.dump(talk, allow_unicode=True))
+        fp.write(yaml.dump(t["meta"], allow_unicode=True))
         fp.write("---\n\n")
-        fp.write(row["Resumo"])
-
+        fp.write(t["abstract"])
 
 # Generate speakers files
 # #######################
 
-speakers = pd.read_csv("palestrantes.csv", dtype=str, keep_default_na=False)
-
-for i, row in speakers.iterrows():
-
-    nome = row["Nome"].strip()
-    sobrenome = row["Sobrenome"].strip()
-    
-    speaker = {"first_name": nome,
-               "last_name": sobrenome,
-               "name": (nome + " " + sobrenome).strip(),
-               "hide": False}
+for i, s in enumerate(speakers.values()):
 
     with open("../_speakers/speaker{0:02d}.md".format(i), "w") as fp:
 
         fp.write("---\n")
-        fp.write(yaml.dump(speaker, allow_unicode=True))
+        fp.write(yaml.dump(s, allow_unicode=True))
         fp.write("\n---\n\n")
-        fp.write(row["Instituicao"])
